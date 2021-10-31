d219d9ab17d3930ae2cbaa63ad93eeba4740d861
frommindtopage <93480981+frommindtopage@users.noreply.github.com>
2021-10-31 20:26:47 +0530

    Update secret language.py

--- secret language.py
+++ secret language.py
@@ -1,5 +1,23 @@
 import pickle
 def NS(s):
+      ------------------------------------
+   Press STOP to exit the program!
+  ------------------------------------
+"""
+from turtle import *
+from datetime import datetime
+
+def jump(distanz, winkel=0):
+    penup()
+    right(winkel)
+    forward(distanz)
+    left(winkel)
+    pendown()
+
+def hand(laenge, spitze):
+    fd(laenge*1.15)
+    rt(90)
+    fd(spitze/2.0)
     f=open("secret.dat","wb")
     pickle.dump(s,f)
     f.close()
@@ -11,6 +29,24 @@ def SN(s):
     f.close()
     f=open("secret.dat","rb")
     c=pickle.load(f)
+  ------------------------------------
+   Press STOP to exit the program!
+  ------------------------------------
+"""
+from turtle import *
+from datetime import datetime
+
+def jump(distanz, winkel=0):
+    penup()
+    right(winkel)
+    forward(distanz)
+    left(winkel)
+    pendown()
+
+def hand(laenge, spitze):
+    fd(laenge*1.15)
+    rt(90)
+    fd(spitze/2.0)
     return c
 
     
